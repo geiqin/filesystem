7043574275716c2b7182e561bd576879f90224fd
tangyonghai <tech@geiqin.com>
2023-04-14 18:37:58 +0800

    modify initial for getname

--- initial.go
+++ initial.go
@@ -1,6 +1,11 @@
 package filesystem
 
-import "github.com/geiqin/gotools/helper"
+import (
+	"fmt"
+	"github.com/geiqin/gotools/helper"
+	"path"
+	"strings"
+)
 
 var fileTypes map[string][]string
 
@@ -27,4 +32,21 @@ func GetStoreSavePath(storeId int64, suffix string) string {
 		flag = "common/"
 	}
 	return flag + suffix
+}
+
+//获得文件扩展名
+func GetFileExtName(fileName string) string {
+	ext := path.Ext(fileName)
+	extName, _ := helper.Substr(ext, 1, len(ext))
+	if extName != "" {
+		extName = strings.ToLower(extName)
+	}
+	return extName
+}
+
+//获取新的文件名称（唯一文件名称）
+func GetNewUniqueFilename(oldFilename string) string {
+	ext := path.Ext(oldFilename)
+	newFileName := fmt.Sprintf("%s%s", helper.MD5(helper.UniqueId()), ext)
+	return newFileName
 }
